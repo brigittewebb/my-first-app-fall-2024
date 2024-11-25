fbcb03a0efa77d11164312305c3ef6174d43bb9f
brigittewebb <bgw17@georgetown.edu>
2024-11-25 18:06:17 -0500

    Stocks Dashboard with Form

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,12 +5,12 @@
 from flask import Flask
 
 from web_app.routes.home_routes import home_routes
-#from web_app.routes.stocks_routes import stocks_routes
+from web_app.routes.stocks_routes import stocks_routes # Need to do this for each routes file
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(home_routes)
-    #app.register_blueprint(stocks_routes)
+    app.register_blueprint(stocks_routes) # Need to do this for each routes file
     return app
 
 if __name__ == "__main__":
