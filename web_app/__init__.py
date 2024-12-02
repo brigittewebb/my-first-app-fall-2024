c97113bc21ebdf37f50ffdbb44e21dbb16d09611
brigittewebb <bgw17@georgetown.edu>
2024-12-02 17:29:21 -0500

    Web App (#4)
    * Setup Flask App (Basic Routing)
    
    * Render HTML template
    
    * Pass variables from route to page
    
    * Shared Layout
    
    * Bootstrap styles
    
    * Stocks Dashboard with Form
    
    * Flash Messaging

---
+++ web_app/__init__.py
@@ -0,0 +1,22 @@
+# this is the "web_app/__init__.py" file...
+
+# This code allows you to import routes from other files within the directory
+
+import os
+from flask import Flask
+
+from web_app.routes.home_routes import home_routes
+from web_app.routes.stocks_routes import stocks_routes # Need to do this for each routes file
+
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
+
+def create_app():
+    app = Flask(__name__)
+    app.config["SECRET_KEY"] = SECRET_KEY
+    app.register_blueprint(home_routes)
+    app.register_blueprint(stocks_routes) # Need to do this for each routes file
+    return app
+
+if __name__ == "__main__":
+    my_app = create_app()
+    my_app.run(debug=True)
