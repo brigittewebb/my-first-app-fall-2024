d735ae4a067878075055d86e32a5564e98231659
brigittewebb <bgw17@georgetown.edu>
2024-12-02 18:20:31 -0500

    Products Page

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -7,6 +7,7 @@ from flask import Flask
 
 from web_app.routes.home_routes import home_routes
 from web_app.routes.stocks_routes import stocks_routes # Need to do this for each routes file
+from web_app.routes.product_routes import product_routes
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
 
@@ -15,6 +16,7 @@ def create_app():
     app.config["SECRET_KEY"] = SECRET_KEY
     app.register_blueprint(home_routes)
     app.register_blueprint(stocks_routes) # Need to do this for each routes file
+    app.register_blueprint(product_routes)
     return app
 
 if __name__ == "__main__":
