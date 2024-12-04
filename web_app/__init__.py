fc90bfda3e87d9bb136652dd6dbeeacbe2fdc2e1
brigittewebb <bgw17@georgetown.edu>
2024-12-04 18:01:35 -0500

    Unemployment Dashboard Implementation

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -8,6 +8,7 @@ from flask import Flask
 from web_app.routes.home_routes import home_routes
 from web_app.routes.stocks_routes import stocks_routes # Need to do this for each routes file
 from web_app.routes.product_routes import product_routes
+from web_app.routes.unemployment_routes import unemployment_routes
 
 SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
 
@@ -17,6 +18,7 @@ def create_app():
     app.register_blueprint(home_routes)
     app.register_blueprint(stocks_routes) # Need to do this for each routes file
     app.register_blueprint(product_routes)
+    app.register_blueprint(unemployment_routes)
     return app
 
 if __name__ == "__main__":
